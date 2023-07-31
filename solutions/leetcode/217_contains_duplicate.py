1cfed0024cbd7bc0d9ec1421a409f32517b03523
Petr Baev <perry322lol@gmail.com>
2023-07-31 23:05:52 +0400

    Add other solutions for 'Contains Duplicate'
    I was checking Copilot...
    
    Signed-off-by: Petr Baev <perry322lol@gmail.com>

--- solutions/leetcode/217_contains_duplicate.py
+++ solutions/leetcode/217_contains_duplicate.py
@@ -1,5 +1,39 @@
-# https://leetcode.com/problems/contains-duplicate/
+class ContainsDuplicate:
+    """https://leetcode.com/problems/contains-duplicate/"""
 
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(set(nums))!=len(nums)
+    def brutforce(self, nums: list[int]) -> bool:
+        # Time complexity: O(n^2)
+        # Space complexity: O(1)
+
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+
+    def sorting(self, nums: list[int]) -> bool:
+        # Time complexity: O(nlogn)
+        # Space complexity: O(1)
+
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        return False
+
+    def hashset(self, nums: list[int]) -> bool:
+        # Time complexity: O(n)
+        # Space complexity: O(n)
+
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num)
+        return False
+
+    def lengths(self, nums: list[int]) -> bool:
+        # Time complexity: O(n)
+        # Space complexity: O(n)
+
+        return len(set(nums)) != len(nums)
