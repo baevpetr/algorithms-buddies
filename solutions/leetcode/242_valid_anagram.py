87d27659755935c7c37972c4f5deb9232f15d3b6
Petr Baev <perry322lol@gmail.com>
2023-07-31 23:28:53 +0400

    Add other solutions for 'Valid Anagram'
    Still tabbing...
    
    Signed-off-by: Petr Baev <perry322lol@gmail.com>

--- solutions/leetcode/242_valid_anagram.py
+++ solutions/leetcode/242_valid_anagram.py
@@ -1,23 +1,42 @@
-# https://leetcode.com/problems/valid-anagram/
-
-from collections import Counter, dafaultdict
+from collections import Counter, defaultdict
 
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        # 1 Solved in 10 seconds
-        return Counter(s) == Counter(t)
+class ValidAnagram:
+    """https://leetcode.com/problems/valid-anagram/"""
 
-        # 2 Counting again...
-        if len(s) != len(t):  # may check it in every solution
-            return False
+    def opt_1(self, s: str, t: str) -> bool:
+        # Strings have to be the same length
+        return len(s) != len(t)
 
-        for char in set(s):  # maybe it's better to generate set from shortest string
-            if s.count(char) != t.count(char):
+    def sorting(self, s: str, t: str) -> bool:
+        # Time complexity: O(nlogn)
+        # Space complexity: O(1)
+
+        return sorted(s) == sorted(t)
+
+    def counting_1(self, s: str, t: str) -> bool:
+        # Time complexity: O(n)
+        # Space complexity: O(n)
+
+        count_s, count_t = {}, {}
+        for i in range(len(s)):
+            count_s[s[i]] = count_s.get(s[i], 0) + 1
+            count_t[t[i]] = count_t.get(t[i], 0) + 1
+        for c in count_s:
+            if count_s[c] != count_t.get(c, 0):
                 return False
         return True
 
-        # 3 With defaultdicts. Almost identical to the first solution, but
+    def counting_2(self, s: str, t: str) -> bool:
+        # Time complexity: O(n)
+        # Space complexity: O(n)
+
+        return Counter(s) == Counter(t)
+
+    def counting_3(self, s: str, t: str) -> bool:
+        # Time complexity: O(n)
+        # Space complexity: O(n)
+
         tracker = defaultdict(int)
         for x in s: tracker[x] += 1
         for x in t: tracker[x] -= 1
